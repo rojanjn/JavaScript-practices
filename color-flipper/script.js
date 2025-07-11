cda5fa51750190d2c3c948d75073d1ecf9615829
Rojan Jafarnezhad <101561560@georgebrown.ca>
2025-07-11 20:31:14 +0330

    final commit

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,3 +1,18 @@
 const colors = [
-    "green", "red", "blue"
-]
+    "#DCD0A8", "#78B9B5", "#DC3C22", "#CC66DA", "#FAEB92", "#819067", "#63C8FF", "#BA487F", "#C83F12", "#DB8DD0"
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+
+    const randomNumber = getRandNum();
+    document.body.style.backgroundColor = colors[randomNumber];
+
+    color.textContent = colors[randomNumber];
+});
+
+function getRandNum() {
+    return Math.floor(Math.random() * colors.length);
+}
